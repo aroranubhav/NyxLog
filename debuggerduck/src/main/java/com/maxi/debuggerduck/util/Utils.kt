2cb818ab715eb04309a2657b9d2e5a3eef0446c8
maxi <arora.anubhav93@gmail.com>
2025-06-28 15:36:42 +0530

    Add method to write logs to file system

--- debuggerduck/src/main/java/com/maxi/debuggerduck/util/Utils.kt
+++ debuggerduck/src/main/java/com/maxi/debuggerduck/util/Utils.kt
@@ -1,10 +1,36 @@
 package com.maxi.debuggerduck.util
 
+import com.google.gson.Gson
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.launch
+import java.io.File
 import java.time.LocalDate
 import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
 
 object Utils {
+
+    fun <T> writeLogsToFile(
+        logs: List<T>,
+        filesDir: File,
+        fileName: String,
+        scope: CoroutineScope
+    ) {
+        val gson = Gson()
+        val logsString = gson.toJson(logs)
+
+        scope.launch {
+            val file = File(
+                filesDir,
+                fileName
+            )
+
+            if (file.exists()) {
+                file.writeText("") //clear file contents
+            }
+            file.appendText(logsString)
+        }
+    }
 
     fun getCurrentDateTime(): String {
         val currentDataTime = LocalDateTime.now()
