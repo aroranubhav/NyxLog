0a0f69decd0b6ea0e7caef7466d974be9ccfb6b6
maxi <arora.anubhav93@gmail.com>
2025-06-28 13:09:09 +0530

    Add constant values

---
+++ debuggerduck/src/main/java/com/maxi/debuggerduck/util/Constants.kt
@@ -0,0 +1,16 @@
+package com.maxi.debuggerduck.util
+
+object Constants {
+
+    const val DEBUGGER_DUCK_DATABASE = "debugger_duck_database"
+    const val VERBOSE_LOGS_TABLE = "debugger_duck_verbose_logs"
+    const val DEBUG_LOGS_TABLE = "debugger_duck_debug_logs"
+    const val INFO_LOGS_TABLE = "debugger_duck_info_logs"
+    const val WARNING_LOGS_TABLE = "debugger_duck_warning_logs"
+    const val ERROR_LOGS_TABLE = "debugger_duck_error_logs"
+    const val VERBOSE_LOGS_FILE_NAME_PREFIX = "debugger_duck_verbose_logs_"
+    const val DEBUG_LOGS_FILE_NAME_PREFIX = "debugger_duck_debug_logs_"
+    const val INFO_LOGS_FILE_NAME_PREFIX = "debugger_duck_info_logs_"
+    const val WARNING_LOGS_FILE_NAME_PREFIX = "debugger_duck_warning_logs_"
+    const val ERROR_LOGS_FILE_NAME_PREFIX = "debugger_duck_error_logs_"
+}
