d0001ac3809a2c9138fc21f2d5ef3173d3985aa6
maxi <arora.anubhav93@gmail.com>
2025-06-28 15:04:36 +0530

    Update Duck class to be a singleton and pass context to it

--- debuggerduck/src/main/java/com/maxi/debuggerduck/Duck.kt
+++ debuggerduck/src/main/java/com/maxi/debuggerduck/Duck.kt
@@ -1,9 +1,45 @@
 package com.maxi.debuggerduck
 
+import android.content.Context
 import android.util.Log
+import com.maxi.debuggerduck.data.local.DebuggerDuckDatabase
+import com.maxi.debuggerduck.data.local.model.Debug
 import com.maxi.debuggerduck.util.LogLevel
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.launch
 
-object Duck {
+class Duck private constructor(
+    context: Context
+) {
+
+    private val appContext: Context
+    private val duckDb: DebuggerDuckDatabase
+    private val job: Job
+    private val scope: CoroutineScope
+
+    init {
+        appContext = context.applicationContext
+        duckDb = DebuggerDuckDatabase.getInstance(appContext)
+        job = Job()
+        scope = CoroutineScope(Dispatchers.IO + job)
+    }
+
+    companion object {
+        @Volatile
+        private var INSTANCE: Duck? = null
+
+        fun getInstance(
+            context: Context
+        ): Duck {
+            return INSTANCE ?: synchronized(this) {
+                INSTANCE ?: Duck(context).also {
+                    INSTANCE = it
+                }
+            }
+        }
+    }
 
     fun v(tag: String, message: String) {
         val logMessage = "${LogLevel.Icon.VERBOSE.icon} $message"
